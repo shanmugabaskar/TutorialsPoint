01be820033755a5d01c16ef768667eede3341ff4
Admin <Admin@JIVIN-DIYA-PC>
2024-10-07 09:08:58 +0530

    Add project files.

---
+++ TutorialsPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Create a new Student object:
+using TutorialsPoint;
+
+Student s = new Student();
+
+// Setting code, name and the age of the student
+s.Code = "001";
+s.Name = "Zara";
+s.Age = 9;
+Console.WriteLine("Student Info: {0}", s);
+
+//let us increase age
+s.Age += 1;
+Console.WriteLine("Student Info: {0}", s);
+Console.ReadKey();
